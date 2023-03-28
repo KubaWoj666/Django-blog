332294a45069dd47a599abe58f54fb98ad11826e
kuba <radicals666@gmail.com>
2023-03-28 15:12:24 +0200

    post-detail and 404 template

--- blog/views.py
+++ blog/views.py
@@ -85,8 +85,9 @@ def starting_page(request):
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html",{"all_posts":all_posts})
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    indentyfy_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {"post": indentyfy_post})
