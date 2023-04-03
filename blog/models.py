09688c178b191cc0da134f6620ba25ce78524220
kuba <radicals666@gmail.com>
2023-04-03 14:43:45 +0200

    Author model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     exert = models.CharField(max_length=200)
@@ -10,4 +17,4 @@ class Post(models.Model):
     # ! db_index=True jest domyślne w polu SlagField i nie trzeba go ustawiać
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
