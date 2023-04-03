d6a32443c26a1ac54876285a87287cf01e69381c
kuba <radicals666@gmail.com>
2023-04-03 13:55:05 +0200

    Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    exert = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    # ! db_index=True jest domyślne w polu SlagField i nie trzeba go ustawiać
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
