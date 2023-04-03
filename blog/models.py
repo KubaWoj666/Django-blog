85bf6672bebd5fe8720ab7aef9702da5b51d1b98
kuba <radicals666@gmail.com>
2023-04-03 14:47:09 +0200

    Tags  model

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -18,3 +22,4 @@ class Post(models.Model):
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)
