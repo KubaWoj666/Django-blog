a1473fd4ff10826416da11f54f5c7b0d5f27903a
kuba <radicals666@gmail.com>
2023-04-11 10:31:41 +0200

    add image fild and uplad settings

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     exert = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     # ! db_index=True jest domyślne w polu SlagField i nie trzeba go ustawiać
     slug = models.SlugField(unique=True, db_index=True)
