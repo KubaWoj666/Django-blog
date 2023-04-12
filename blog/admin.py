293c97f90d173cedb2749d65cbe8eff7e6d131c6
kuba <radicals666@gmail.com>
2023-04-12 10:48:09 +0200

    Display comment ad style comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 
-from .models import Tag, Author, Post
+from .models import Tag, Author, Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tags", "date")
     list_display = ("title", "date", "author")
     prepopulated_fields = {"slug":("title",)}
 
-# Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
 
